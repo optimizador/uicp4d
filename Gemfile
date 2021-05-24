d61faeb063e32a498f76a20fabf6a5d0471667bf
pedro z barrientos <pbarrien@gmail.com>
2021-05-24 18:42:51 -0400

    actualizacion

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 
 # Gemfile
-
 source 'https://rubygems.org'
-
 gem 'sinatra'
+gem 'rerun'
--- basic.rb
+++ basic.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+set(:cookie_options) do
+  { :expires => Time.now + 30*60 }
+end
+get '/' do
+  logger = Logger.new(STDOUT)
+  logger.info(request)
+  #@name = "Pedro"
+  response.set_cookie("llave", value: "valor")
+  erb :index
 
-get "/" do
-  "Hello World!"
+end
+
+get '/nombre' do
+  logger = Logger.new(STDOUT)
+  logger.info("Recibi lo siguiente #{params[:name]}")
+  @name = "#{params[:name]}"
+  response.set_cookie("llave2", value: "valor2")
+  erb :index
 end
