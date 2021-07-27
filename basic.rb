fd68179b1c91a8bb2cde60ad804b687a576d83ea
pedro martinez <pbarrien@gmail.com>
2021-07-27 14:10:34 -0500

    template cp4d

--- basic.rb
+++ basic.rb
@@ -92,9 +92,7 @@ get '/cp4drespuesta' do
   respuestastorage=JSON.parse(respuestastorage.to_s)
   logger.info(respuestastorage)
   logger.info("TERMINO DE LLAMAR LOS APIS")
-  #erb :cp4d , :locals => {:respuestasizing => params[:respuestasizing]}
-  #response['Access-Control-Allow-Origin'] = 'https://menu-dimensionamiento.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/'
-  erb :cp4d , :locals => {:respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
+  erb :cp4d , :locals => { :respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
 end
 
   get '/cp4dtemplate' do
@@ -102,33 +100,119 @@ end
     logger.info("Selecciono dimensionamiento para template de CP4D")
     @name = "CP4D"
     respuestasizing=[]
+    respuestasizingpx=[]
     respuestasizingalt=[]
     respuestastorage=[]
-    #response['Access-Control-Allow-Origin'] = 'https://menu-dimensionamiento.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/'
-    erb :cp4dtemplate , :locals => {:respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
+    respuestasizingga=[]
+    respuestasizingdl=[]
+    respuestasol=[]
+    erb :cp4dtemplate , :locals => {:respuestasol => respuestasol,:respuestasizingdl => respuestasizingdl,:respuestasizingga => respuestasizingga, :respuestasizingpx => respuestasizingpx,:respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
   end
   get '/cp4dtemplaterespuesta' do
-    #urlapi="https://apis.9sxuen7c9q9.us-south.codeengine.appdomain.cloud"
-    urlapi="http://localhost:8080"
+    urlapi="https://apis.9sxuen7c9q9.us-south.codeengine.appdomain.cloud"
+    #urlapi="http://localhost:8080"
+
     logger = Logger.new(STDOUT)
     logger.info("Selecciono dimensionamiento para template de CP4D")
     @name = "CP4D"
 
+    ####################################
+    # Parametros Generales
+    ####################################
+    region="#{params['region']}"
+    country_offer="#{params['preciopais']}"
+    tiposoporte="#{params['tiposoporte']}"
+
+    increspaldos="#{params['increspaldos']}"
+    inclogs="#{params['inclogs']}"
+    inclogs="#{params['incsalud']}"
+    inclogs="#{params['incauditoria']}"
+    inclogs="#{params['incga']}"
+    inclogs="#{params['incdl']}"
+
+    ####################################
+    # Parametros para Clúster
+    ####################################
     cpu="#{params['cpu']}"
     ram="#{params['ram']}"
     infra_type="#{params['infra_type']}"
-    region="#{params['region']}"
     storage="#{params['storage']}"
     iops="#{params['iops']}"
 
+    ####################################
+    # Parametros para Respaldos
+    ####################################
+    logger = Logger.new(STDOUT)
+    logger.info("Recibiendo parametros para dimensionamiento de PX-backup:" +
+      "rsemanal:"+"#{params['rsemanal']}"+
+      "rsemanalretencion:"+ "#{params['rsemanalretencion']}"+
+      "rdiario: "+"#{params['rdiario']}"+
+      "rdiarioretencion:"+"#{params['rdiarioretencion']}"+
+      "rmensual: "+"#{params['rmensual']}"+
+      "rmensualretencion:"+"#{params['rmensualretencion']}"+
+      "ranual: "+"#{params['ranual']}"+
+      "ranualretencion:"+"#{params['ranualretencion']}"+
+      "regioncluster: "+"#{params['regioncluster']}"+
+      "almacenamientogb:"+"#{params['almacenamientogb']}"+
+      "countryrespaldo: "+"#{params['countryrespaldo']}"+
+      "resiliencybackup:"+"#{params['resiliencybackup']}")
 
+
+    almacenamientogb="#{params['almacenamientogb']}" #cantidad en GB
+    #parametros de politicas
+    rsemanal="#{params['rsemanal']}"
+    rsemanalretencion="#{params['rsemanalretencion']}" #cantidad de backups retenidos
+    rdiario="#{params['rdiario']}"
+    rdiarioretencion="#{params['rdiarioretencion']}"#cantidad de backups retenidos
+    rmensual="#{params['rmensual']}"
+    rmensualretencion="#{params['rmensualretencion']}"#cantidad de backups retenidos
+    ranual="#{params['ranual']}"
+    ranualretencion="#{params['ranualretencion']}"#cantidad de backups retenidos
+    regioncluster=region
+    #{}"#{params['regioncluster']}"#region del cluster de IKS donde se desplegará PX-Backup
+    countryrespaldo = "#{params['countryrespaldo']}"
+    resiliencybackup ="#{params['resiliencybackup']}"
+
+
+    ####################################
+    # Parametros para Gateway Appliance
+    ####################################
+    logger.info("Recibiendo parametros para dimensionamiento de GatewayAppliance:")
+    logger.info("Type: #{params[:typega]} Interfase: #{params[:interfase]} PII: #{params[:pii]} HA #{params[:ha]}")
+
+    typega="#{params['typega']}"
+    interfase="#{params['interfase']}"
+    pii="#{params['pii']}"
+    ha="#{params['ha']}"
+    #parametros recibidos
+
+
+    ####################################
+    # Parametros para DirectLink
+    ####################################
+    typedl="#{params['typedl']}"
+
+    puerto="#{params['puerto']}"
+    routing="#{params['routing']}"
+    ha="#{params['ha']}"
+
+    respuestasizingpx=[]
     respuestasizing=[]
     respuestasizingalt=[]
     respuestastorage=[]
+    respuestasizingga=[]
+    respuestasizingdl=[]
+    respuestasol=[]
+    precioservicios=0
+
     #parametros recibidos
     logger.info("PRIMER LLAMADO DE API #{urlapi}/api/v2/sizingclusteroptimo?cpu=#{cpu}&ram=#{ram}&infra_type=#{infra_type}&region=#{region}")
     respuestasizing = RestClient.get "#{urlapi}/api/v2/sizingclusteroptimo?cpu=#{cpu}&ram=#{ram}&infra_type=#{infra_type}&region=#{region}", {:params => {}}
     respuestasizing=JSON.parse(respuestasizing.to_s)
+    preciocluster=precioservicios+respuestasizing[0]["precio"].to_f
+    precioservicios=precioservicios+preciocluster
+    logger.info("Precio Clúster: #{preciocluster}")
+    logger.info("Precio Servicios: #{precioservicios}")
     logger.info(respuestasizing)
     logger.info("SEGUNDO LLAMADO DE API #{urlapi}/api/v2/sizingcluster?cpu=#{cpu}&ram=#{ram}&infra_type=#{infra_type}&region=#{region}")
     respuestasizingalt = RestClient.get "#{urlapi}/api/v2/sizingcluster?cpu=#{cpu}&ram=#{ram}&infra_type=#{infra_type}&region=#{region}", {:params => {}}
@@ -137,8 +221,70 @@ end
     logger.info("TERCER LLAMADO DE API #{urlapi}/api/v1/sizingblockstorage?iops=#{iops}&storage=#{storage}&region=#{region}")
     respuestastorage = RestClient.get "#{urlapi}/api/v1/sizingblockstorage?iops=#{iops}&storage=#{storage}&region=#{region}", {:params => {}}
     respuestastorage=JSON.parse(respuestastorage.to_s)
+    preciostorage=respuestastorage[0]["precio"].to_f+respuestastorage[0]["preciounidadrestante"].to_f
+    precioservicios=precioservicios+preciostorage
+    logger.info("Precio Storage: #{preciostorage}")
+    logger.info("Precio Servicios: #{precioservicios}")
+
     logger.info(respuestastorage)
+
+    #parametros recibidos
+    logger.info("llamado api PX-Backup:" )
+    logger.info("#{urlapi}/api/lvl2/pxbackupsol?almacenamientogb=#{almacenamientogb}&rsemanal=#{rsemanal}&rsemanalretencion=#{rsemanalretencion}&rdiario=#{rdiario}&rdiarioretencion=#{rdiarioretencion}&rmensual=#{rmensual}&rmensualretencion=#{rmensualretencion}&ranual=#{ranual}&ranualretencion=#{ranualretencion}&regioncluster=#{regioncluster}&countryrespaldo=#{countryrespaldo}&resiliencybackup=#{resiliencybackup}")
+    respuestasizingpx = RestClient.get "#{urlapi}/api/lvl2/pxbackupsol?almacenamientogb=#{almacenamientogb}&rsemanal=#{rsemanal}&rsemanalretencion=#{rsemanalretencion}&rdiario=#{rdiario}&rdiarioretencion=#{rdiarioretencion}&rmensual=#{rmensual}&rmensualretencion=#{rmensualretencion}&ranual=#{ranual}&ranualretencion=#{ranualretencion}&regioncluster=#{regioncluster}&countryrespaldo=#{countryrespaldo}&resiliencybackup=#{resiliencybackup}", {:params => {}}
+    respuestasizingpx = JSON.parse(respuestasizingpx.to_s)
+    logger.info("*************")
+    logger.info(respuestasizingpx)
+    precioiks=respuestasizingpx[1]["precio"]
+    preciocos=respuestasizingpx[3]["precio"]
+    preciopx=respuestasizingpx[2]["precio"]
+    precioservicios=precioservicios+preciopx+preciocos+precioiks
+    logger.info("Precio Sol PX: IKS #{precioiks} COS #{preciocos} PX #{preciopx}")
+    logger.info("Precio Servicios: #{precioservicios}")
+
+    urlapiga="https://apis-ga.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/"
+    #urlapiga="http://localhost:8080"
+    logger.info("llamado api GA:" )
+    logger.info("#{urlapiga}/api/v1/sizingga?region=#{region}&type=#{typega}&interfase=#{interfase}&ha=#{ha}&pii=#{pii}")
+    respuestasizingga = RestClient.get "#{urlapiga}/api/v1/sizingga?region=#{region}&type=#{typega}&interfase=#{interfase}&ha=#{ha}&pii=#{pii}", {:params => {}}
+    respuestasizingga=JSON.parse(respuestasizingga.to_s)
+    logger.info("*************")
+    logger.info(respuestasizingga)
+    precioga=respuestasizingga[0]["precio"].to_f
+    precioservicios=precioservicios+precioga
+    logger.info("Precio Gateway Appliance: #{precioga}")
+    logger.info("Precio Servicios: #{precioservicios}")
+
+
+
+    logger.info("llamado api DL:" )
+    respuestasizingdl = RestClient.get "#{urlapiga}/api/v1/sizingdl?region=#{region}&type=#{typedl}&country_offer=#{country_offer}&puerto=#{puerto}&routing=#{routing}&ha=#{ha}", {:params => {}}
+    respuestasizingdl=JSON.parse(respuestasizingdl.to_s)
+    logger.info("*************")
+    logger.info(respuestasizingdl)
+    preciodl=respuestasizingdl[0]["precio"].to_f
+    precioservicios=precioservicios+preciodl
+    logger.info("Precio Direct Link: #{preciodl}")
+    logger.info("Precio Servicios: #{precioservicios}")
     logger.info("TERMINO DE LLAMAR LOS APIS")
+
+    respuestasoporte=[]
+    llamadoapisoporte="#{urlapi}/api/v1/sizingsupport?type=#{tiposoporte}&precioservicios=#{precioservicios}"
+    logger.info("Llamado API Soporte #{llamadoapisoporte}")
+    respuestasoporte = RestClient.get llamadoapisoporte, {:params => {}}
+    logger.info("Respuesta API Soporte #{respuestasoporte}")
+    respuestasoporte=JSON.parse(respuestasoporte.to_s)
+    preciosoporte=respuestasoporte["precio"].to_f
+    preciototal=preciosoporte+precioservicios
+    respuestasol=[]
+    respuestasol.push({total:preciototal.round(2).to_s,soporte:preciosoporte.round(2).to_s,servicios:precioservicios.round(2).to_s})
+    #respuestasol=respuestasol.to_json
+    logger.info("Precio Servicios: #{precioservicios}")
+    logger.info("Precio Soporte: #{preciosoporte}")
+    logger.info("Precio Total: #{preciototal}")
+
+    logger.info("TERMINO DE LLAMAR LOS APIS")
+
     #response['Access-Control-Allow-Origin'] = 'https://menu-dimensionamiento.9sxuen7c9q9.us-south.codeengine.appdomain.cloud/'
-    erb :cp4dtemplate , :locals => {:respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
+    erb :cp4dtemplate , :locals => {:respuestasol => respuestasol,:respuestasizingdl => respuestasizingdl, :respuestasizingga => respuestasizingga,:respuestasizingpx => respuestasizingpx, :respuestasizing => respuestasizing,:respuestasizingalt => respuestasizingalt, :respuestastorage => respuestastorage}
   end
